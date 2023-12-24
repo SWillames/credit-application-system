311223350743b16e7bd5d85f1b366f5d33d80670
Sergio Costa <tp.sergio.willames@totvspartners.com.br>
2023-12-24 20:31:06 -0300

    project created.

---
+++ src/main/kotlin/swsoftware/creditaplicationsystem/CreditAplicationSystemApplication.kt
@@ -0,0 +1,11 @@
+package swsoftware.creditaplicationsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditAplicationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditAplicationSystemApplication>(*args)
+}
---
+++ src/test/kotlin/swsoftware/creditaplicationsystem/CreditAplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package swsoftware.creditaplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
