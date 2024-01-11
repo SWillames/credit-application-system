e53aa1c1b1c060b6a95eef90aaa7035d2c2f674c
Sergio Costa <sergiowoc@gmail.com>
2024-01-11 11:18:15 -0300

    project created.

---
+++ src/test/kotlin/br/com/sw_software/creditapplicationsystem/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.sw_software.creditapplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
